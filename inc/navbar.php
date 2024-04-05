60a59fac2f1e057842e68afb6946893e792d4596
Hans Pariona <hparionalazo123@gmail.com>
2024-04-05 10:37:13 -0500

    cambios para categoria

--- inc/navbar.php
+++ inc/navbar.php
@@ -38,10 +38,10 @@
           <a class="navbar-link">Productos</a>
 
           <div class="navbar-dropdown">
-              <a class="navbar-item">Nueva</a>
-              <a class="navbar-item">Lista</a>
-              <a class="navbar-item">Por categorias</a>
-              <a class="navbar-item">Buscar</a>
+              <a class="navbar-item" href="index.php?vista=product_new">Nueva</a>
+              <a class="navbar-item" href="index.php?vista=product_list">Lista</a>
+              <a class="navbar-item" href="index.php?vista=product_category">Por categorias</a>
+              <a class="navbar-item" href="index.php?vista=product_search">Buscar</a>
           </div>
       </div>
   </div>
